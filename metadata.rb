a382a06415a7ad086f3d14b669b65da3103d9c1c
David Joos <david.joos@gmail.com>
2013-01-17 11:13:45 +0000

    Upgrade PEAR unconditionally

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "dev@escapestudios.com"
 license          "MIT"
 description      "Installs/Configures phpunit"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.2"
+version          "0.0.3"
 
 supports "ubuntu"
 
--- recipes/pear.rb
+++ recipes/pear.rb
@@ -12,11 +12,9 @@ php_pear_channel "pear.php.net" do
   action :update
 end
 
-#"skip" issues with pear 1.9.0 & 1.9.2 by upgrading PEAR
+#upgrade PEAR
 php_pear "pear" do
-	cur_version = `pear -V| head -1| awk -F': ' '{print $2}'`
 	action :upgrade
-	not_if { Gem::Version.new(cur_version) > Gem::Version.new('1.9.0') }
 end
 
 #a component that PHPUnit depends upon is hosted on the Symfony2 PEAR channel
