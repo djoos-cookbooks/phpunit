f8a1e000ccce843ef08b4b5e2e1d3cd1e52c4196
David Joos <david.joos@gmail.com>
2013-03-13 19:24:33 +0000

    metadata attribute change(s)

--- metadata.rb
+++ metadata.rb
@@ -1,17 +1,18 @@
+name             "phpunit"
 maintainer       "Escape Studios"
 maintainer_email "dev@escapestudios.com"
 license          "MIT"
 description      "Installs/Configures PHPUnit"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.6"
+version          "0.0.7"
 
-supports "ubuntu"
-supports "debian"
-supports "centos"
-supports "redhat"
-supports "fedora"
-supports "scientific"
-supports "amazon"
+%w{ debian ubuntu redhat centos fedora scientific amazon }.each do |os|
+supports os
+end
 
 depends "php"
 depends "composer"
+
+recipe "phpunit", "Installs phpunit."
+recipe "phpunit::composer", "Installs phpunit using composer."
+recipe "phpunit::pear", "Installs phpunit using pear."
