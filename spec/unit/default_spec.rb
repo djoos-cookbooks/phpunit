4a4565a6040097222e493c0ab700f1e513e63bee
Michael Misshore <michael@mudbugmedia.com>
2014-05-22 18:33:10 -0500

    Update default recipe and ensure it uses the default recipe

--- spec/unit/default_spec.rb
+++ spec/unit/default_spec.rb
@@ -1,5 +1,19 @@
 require 'spec_helper'
 
 describe 'phpunit::default' do
-  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+  let(:install_method) { 'composer' }
+
+  let(:chef_run) do
+    ChefSpec::Runner.new do |node|
+      node.set[:phpunit][:install_method] = install_method
+    end.converge(described_recipe)
+  end
+
+  before do
+    stub_command("php -m | grep 'Phar'").and_return('')
+  end
+
+  it 'includes the phpunit::composer recipe' do
+    expect(chef_run).to include_recipe('phpunit::composer')
+  end
 end
